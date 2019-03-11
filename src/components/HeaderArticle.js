aa402ce4ece2fee14257fcab2071eadf8b336631
trastanechora <trastanechora@gmail.com>
2019-03-11 18:44:42 +0700

    changed navbar

--- src/components/HeaderArticle.js
+++ src/components/HeaderArticle.js
@@ -6,36 +6,65 @@ import '../css/style.css';
 class HeaderArticle extends Component {
   render() {
     return (
-      <header className="header1">
-        <div className="row head-row">
-          <div className="col-md-1"></div>
-          <div className="col-md-2 row">
-            <div className="col-md-4"><img src={require("../logo/logo.svg")}/></div>
-            <div className="col-md-8">KabarKabar</div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light header1">
+          {/* <a class="navbar-brand" href="#">Navbar</a> */}
+          <img className="nav-logo" src={require("../logo/logo.svg")}/>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">KabarKabar <span class="sr-only">(current)</span></a>
+              </li>
+            </ul>
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Sepakbola</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Ekonomi</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Politik</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Hiburan</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Lainnya
+                </a>
+
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+
+              </li>
+            </ul>
+            <ul class="navbar-nav mr-auto">
+              <li>
+                <form class="form-inline my-2 my-lg-0">
+                  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Q</button>
+                </form>
+              </li>
+            </ul>
+            <ul class="navbar-nav mr-auto">
+              <li>
+                <a class="nav-link" href="#">Masuk</a>
+              </li>
+              <li>
+                <a class="nav-link" href="#">Daftar</a>
+              </li>
+            </ul>
+            
           </div>
-          <div className="col-md-4 row">
-            <div className="col-md-3">Sepakbola</div>
-            <div className="col-md-2">Ekonomi</div>
-            <div className="col-md-2">Politik</div>
-            <div className="col-md-2">Hiburan</div>
-            <div className="col-md-2">Lainnya</div>
-          </div>
-          <div className="col-md-3 row">
-            <div className="col-md-8">
-            <div class="input-group mb-3">
-              <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2" />
-              <div class="input-group-append">
-                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Q</button>
-              </div>
-            </div>
-            </div>
-          </div>
-          <div className="col-md-2 row">
-            <div className="col-md-6">Masuk</div>
-            <div className="col-md-6">Daftar</div>
-          </div>
-        </div>
-      </header>
+        </nav>
     );
   }
 }
--- src/components/Top.js
+++ src/components/Top.js
@@ -11,23 +11,23 @@ class Top extends Component {
           <div className="col-md-5 link-top"><a href="#">Lihat semua</a></div>
         </div>
         {/* Content top articles */}
-        <div className="col-md-12 row box">
+        <div className="col-md-12 row box list">
           <div className="number-top">#1</div>
           <div className="col-md-12"><p className="top-paragraf">Gabung Alphatech Academy Sekarang!</p></div> 
         </div>
-        <div className="col-md-12 row box">
+        <div className="col-md-12 row box list">
           <div className="number-top">#2</div>
           <div className="col-md-12"><p className="top-paragraf">Ada apa antara Kobar dan Hasan?</p></div> 
         </div>
-        <div className="col-md-12 row box">
+        <div className="col-md-12 row box list">
           <div className="number-top">#3</div>
           <div className="col-md-12"><p className="top-paragraf">Mengenal Arafat sang Master Python</p></div> 
         </div>
-        <div className="col-md-12 row box">
+        <div className="col-md-12 row box list">
           <div className="number-top">#4</div>
           <div className="col-md-12"><p className="top-paragraf">Belajar reactJS itu seru. Kamu setuju?</p></div> 
         </div>
-        <div className="col-md-12 row box">
+        <div className="col-md-12 row box list">
           <div className="number-top">#5</div>
           <div className="col-md-12"><p className="top-paragraf">Sudahkah kamu sehat mental?</p></div> 
         </div>
